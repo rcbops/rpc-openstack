0d2a1dbac02a30aa009fc90a6a3d2fc899bd97da
Matt Thompson <mattt@defunct.ca>
2013-08-22 10:43:35 +0100

    Take into account ram_allocation_ratio

--- capacity.py
+++ capacity.py
@@ -1,24 +1,33 @@
 #!/usr/bin/env python
 
-# idea borrowed from nova/cells/state.py
-# note that this does not take into account ram_allocation_ratio which by
-# default allows memory overcommitting
-
 from nova import config
 from nova import context
 from nova.db import api
+from nova.scheduler import host_manager
+from nova.scheduler.filters import ram_filter
 
-config.parse_args([])
-ctxt = context.get_admin_context()
-compute_hosts = api.compute_node_get_all(ctxt)
 
-total_ram_mb_free = 0
-total_disk_mb_free = 0
+def main():
+    config.parse_args([])
+    ctxt = context.get_admin_context()
+    compute_hosts = api.compute_node_get_all(ctxt)
 
-for compute_host in compute_hosts:
-    total_ram_mb_free += compute_host['free_ram_mb']
-    total_disk_mb_free += compute_host['free_disk_gb'] * 1024
+    total_ram_mb_free = 0
+    total_disk_mb_free = 0
 
-print "status success"
-print "metric total_ram_mb_free int64 %d" % total_ram_mb_free
-print "metric total_disk_mb_free int64 %d" % total_disk_mb_free
+    for compute_host in compute_hosts:
+        if 'RamFilter' in host_manager.CONF.scheduler_default_filters:
+            total_ram_mb_free += (compute_host['free_ram_mb'] *
+                                  ram_filter.CONF.ram_allocation_ratio)
+        else:
+            total_ram_mb_free += compute_host['free_ram_mb']
+
+        total_disk_mb_free += compute_host['free_disk_gb'] * 1024
+
+    print "status success"
+    print "metric total_ram_mb_free int64 %d" % total_ram_mb_free
+    print "metric total_disk_mb_free int64 %d" % total_disk_mb_free
+
+
+if __name__ == "__main__":
+    main()
