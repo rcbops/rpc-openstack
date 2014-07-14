d26117e5b183cd106e9d5b4439fdc8dd1f7884eb
Ian Cordasco <ian.cordasco@rackspace.com>
2014-07-14 11:38:51 -0500

    Add cache item counting

--- memcached_status.py
+++ memcached_status.py
@@ -1,18 +1,39 @@
 #!/usr/bin/env python
 import os
+import re
 import socket
 import subprocess
 import telnetlib
 
 
-def can_connect(host, port):
-    """Check that we can make a connection to Memcached."""
+ITEM_COUNT = re.compile('STAT items:\d+:number (\d+)')
+
+
+def item_stats(host, port):
+    """Check the stats for items and connection status."""
     try:
         c = telnetlib.Telnet(host, port)
     except socket.error:
-        return False
+        return None
+    c.write('stats items\r\n')
+    results = c.read_until('END').strip('END')
     c.close()
-    return True
+    return results
+
+
+def item_count(results):
+    """Find the number of items in the cache."""
+    if not results:
+        return 0
+
+    count = 0
+    for result in results.split('\r\n'):
+        m = ITEM_COUNT.match(result)
+        if m is None:
+            continue
+        count += int(m.groups()[0], base=10)
+
+    return count
 
 
 def main():
@@ -21,9 +42,17 @@ def main():
 
     hostname = subprocess.check_output('hostname').strip()
     localhost = '127.0.0.1'
+    port = '11211'
 
-    if can_connect(hostname, '11211') or can_connect(localhost, '11211'):
+    results = item_stats(hostname, port)
+    if results is None:
+        # If connecting to hostname is not possible, try connecting to
+        # localhost
+        results = item_stats(localhost, port)
+
+    if results is not None:
         print 'status ok memcached is reachable'
+        print 'metric items_in_cache {0}'.format(item_count(results))
     else:
         print 'status err memcached is unreachable'
 
