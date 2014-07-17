b985f727283eab3a66eed5359fbfa95b1ce1194d
Ian Cordasco <ian.cordasco@rackspace.com>
2014-07-17 13:50:00 -0500

    Only print the namespace that is misbehaving

--- tap_dhcp.py
+++ tap_dhcp.py
@@ -30,8 +30,9 @@ def main():
         named_interfaces = filter(lambda i: TAP.match(i), interface_list)
         num_taps = len([i for i in named_interfaces if not LOOP.match(i)])
         if num_taps != 1:
-            print 'status err namespace {0} has {1} TAPs present'.format(
-                namespaces, num_taps)
+            print ('status err namespace {0} has {1} TAPs present'
+                   ' (expected only 1 TAP to be present)').format(
+                namespace, num_taps)
             errored = True
 
     if not errored:
