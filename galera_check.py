1cac045e45153422e0fa3d90db1f6d4177e7a8ff
Ian Cordasco <ian.cordasco@rackspace.com>
2014-07-17 16:06:53 -0500

    Send Cluster & Local State UUID to MaaS

--- galera_check.py
+++ galera_check.py
@@ -51,4 +51,8 @@ if OUTPUT != "":
                "metric WSREP_CLUSTER_SIZE int "
                + SLAVE_STATUS["wsrep_cluster_size"] + "\n"
                "metric QUERIES_PER_SECOND int "
-               + SLAVE_STATUS["Queries"])
+               + SLAVE_STATUS["Queries"] + "\n"
+               "metric WSREP_CLUSTER_STATE_UUID str "
+               + SLAVE_STATUS["wsrep_cluster_state_uuid"] + "\n"
+               "metric WSREP_LOCAL_STATE_UUID str "
+               + SLAVE_STATUS["wsrep_local_state_uuid"] + "\n")
