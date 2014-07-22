c3ef1d6d2cf665a7fd9b4b805c2e327595fabecc
Ian Cordasco <ian.cordasco@rackspace.com>
2014-07-22 09:57:45 -0500

    Simple refactor

--- elasticsearch.py
+++ elasticsearch.py
@@ -20,26 +20,14 @@ def find_indices(address):
     return sorted(res['_index'] for res in r.json()['hits']['hits'])
 
 
-def get_number_of_errors(address, index):
-    """Retrieve the number of logs with level ERROR."""
+def get_number_of(loglevel, address, index):
+    """Retrieve the number of logs with level ``loglevel``."""
     url = 'http://{0}:9200/{1}/_search'.format(address, index)
     r = requests.get(url, data=json.dumps({
         'query': {
             'query_string': {
-                'query': 'ERROR'
-            }
-        }
-    }))
-    return r.json()['hits']['total']
-
-
-def get_number_of_warnings(address, index):
-    """Retrieve the number of logs with level ERROR."""
-    url = 'http://{0}:9200/{1}/_search'.format(address, index)
-    r = requests.get(url, data=json.dumps({
-        'query': {
-            'query_string': {
-                'query': 'WARN'
+                'query': loglevel,
+                'fields': ['os_level', 'message']
             }
         }
     }))
@@ -49,8 +37,8 @@ def get_number_of_warnings(address, index):
 def main():
     address = get_elasticsearch_address()
     latest = find_indices(address)[-1]
-    num_errors = get_number_of_errors(address, latest)
-    num_warnings = get_number_of_warnings(address, latest)
+    num_errors = get_number_of('ERROR', address, latest)
+    num_warnings = get_number_of('WARN*', address, latest)
     print 'metric int NUMBER_OF_LOG_ERRORS {0}'.format(num_errors)
     print 'metric int NUMBER_OF_LOG_WARNINGS {0}'.format(num_warnings)
 
