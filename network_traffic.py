076ac15824c369c15d1b244071459d003ce5c411
Ian Cordasco <ian.cordasco@rackspace.com>
2014-07-22 13:16:40 -0500

    Comment and refactor the network monitor

--- network_traffic.py
+++ network_traffic.py
@@ -3,7 +3,7 @@ import re
 
 # In the event we need more information, let's name the information
 Interface = collections.namedtuple(
-    'Interface',
+    'Interface',  # Name of the named tuple
     ('name',  # Interface name, e.g., veth1064K4
      # Receive information
      'rcv_bytes', 'rcv_packets', 'rcv_errs', 'rcv_drop', 'rcv_fifo',
@@ -16,13 +16,24 @@ Interface = collections.namedtuple(
 SPLIT_RE = re.compile(':?\s+')
 
 
+class Interface(Interface):
+    @classmethod
+    def from_str(cls, info):
+        """
+        Take a string from the network device list and split it into a tuple.
+        """
+        # Strip leading whitespace and trailing newlines and then split into
+        # components of the Interface.
+        return cls(*SPLIT_RE.split(info.strip()))
+
+
 def network_info():
     """Retrieve a list of Interface objects to use to report metrics."""
     with open('/proc/net/dev', 'r') as fd:
         # The first two lines of this file are not important to us
         lines = fd.readlines()[2:]
 
-    return [Interface(*SPLIT_RE.split(l.strip())) for l in lines]
+    return map(Interface.from_str, lines)
 
 
 def as_kb(num_bytes):
@@ -30,22 +41,14 @@ def as_kb(num_bytes):
     return int(num_bytes, base=10) / 1024
 
 
-def rcv_kb(interface):
-    return as_kb(interface.rcv_bytes)
-
-
-def tx_kb(interface):
-    return as_kb(interface.tx_bytes)
-
-
 def main():
     """The heart of the program."""
     for interface in network_info():
         print 'metric {0}_KB_RECEIVED int32 {1}'.format(
-            interface.name, rcv_kb(interface)
+            interface.name, as_kb(interface.rcv_bytes)
         )
         print 'metric {0}_KB_SENT int32 {1}'.format(
-            interface.name, tx_kb(interface)
+            interface.name, as_kb(interface.tx_bytes)
         )
 
 
