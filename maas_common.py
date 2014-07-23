3b55c2b041e98f6cdf4f49b8811a86393f77d0f9
Matt Thompson <mattt@defunct.ca>
2014-07-23 14:50:32 +0100

    Parse /root/openrc instead of using os.environ
    This change updates maas_common.py to use auth credentials in
    /root/openrc instead of finding them in os.environ.  The benefit here
    is that we do not have to source /root/openrc before running the plugin
    (which I'm not even sure how you'd do that from MaaS).

--- maas_common.py
+++ maas_common.py
@@ -1,24 +1,33 @@
 #!/usr/bin/env python
 
-import os
+import re
 import sys
 
-# We can set these here to avoid looking in os.environ
 AUTH_DETAILS = {'OS_USERNAME': None,
                 'OS_PASSWORD': None,
                 'OS_TENANT_NAME': None,
                 'OS_AUTH_URL': None}
 
+OPENRC = '/root/openrc'
+
 
 def set_auth_details():
     auth_details = AUTH_DETAILS
+    pattern = re.compile('^(export\s)?(?P<key>\w+)(\s+)?=(\s+)?(?P<value>.*)$')
+
+    openrc = open(OPENRC)
+    for line in openrc:
+        match = pattern.match(line)
+        if match:
+            k = match.group('key')
+            v = match.group('value')
+            if k in auth_details and auth_details[k] is None:
+                auth_details[k] = v
+    openrc.close()
 
     for key in auth_details.keys():
-        if AUTH_DETAILS[key] is None:
-            if key in os.environ:
-                auth_details[key] = os.environ[key]
-            else:
-                print "status err os.environ['%s'] not set" % key
-                sys.exit(1)
+        if auth_details[key] is None:
+            print "status err %s not set" % key
+            sys.exit(1)
 
     return auth_details
