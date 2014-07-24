82376ed887a1b47849d5fcaa7bfb29831b5c685a
Ian Cordasco <ian.cordasco@rackspace.com>
2014-07-24 10:08:25 -0500

    Use sys.exit and flatten the main function

--- horizon_check.py
+++ horizon_check.py
@@ -12,7 +12,6 @@ HORIZON_PORT = '443'
 
 
 def main():
-
     splash_status_code = 0
     splash_seconds = 0.0
     login_status_code = 0
@@ -28,43 +27,50 @@ def main():
         r = s.get('%s:%s' % (HORIZON_URL, HORIZON_PORT),
                   verify=False,
                   timeout=10)
-
-        splash_status_code = r.status_code
-        splash_seconds = r.elapsed.total_seconds()
-        if (r.status_code == requests.codes.ok and
-                re.search('openstack dashboard', r.content, re.IGNORECASE)):
-            csrf_token = html.fromstring(r.content).xpath(
-                '//input[@name="csrfmiddlewaretoken"]/@value')[0]
-            region = html.fromstring(r.content).xpath(
-                '//input[@name="region"]/@value')[0]
-            s.headers.update(
-                {'Content-type': 'application/x-www-form-urlencoded',
-                    'Referer': HORIZON_URL})
-            payload = {'username': OS_USERNAME,
-                       'password': OS_PASSWORD,
-                       'csrfmiddlewaretoken': csrf_token,
-                       'region': region}
-            l = s.post(
-                ('%s:%s/auth/login/') % (HORIZON_URL, HORIZON_PORT),
-                data=payload,
-                verify=False)
-            login_status_code = l.status_code
-            login_seconds = l.elapsed.total_seconds()
-            if (l.status_code == requests.codes.ok and
-                    re.search('overview', l.content, re.IGNORECASE)):
-                print 'status OK'
-            else:
-                print 'status err could not log in'
-                sys.ext(1)
-        else:
-            print 'status err could not load login page'
-            sys.ext(1)
-
     except (exc.ConnectionError,
             exc.HTTPError,
             exc.Timeout) as e:
         print 'status err %s ' % e
+        sys.exit(1)
 
+    if not (r.ok and
+            re.search('openstack dashboard', r.content, re.IGNORECASE)):
+        print 'status err could not load login page'
+        sys.exit(1)
+
+    splash_status_code = r.status_code
+    splash_seconds = r.elapsed.total_seconds()
+
+    csrf_token = html.fromstring(r.content).xpath(
+        '//input[@name="csrfmiddlewaretoken"]/@value')[0]
+    region = html.fromstring(r.content).xpath(
+        '//input[@name="region"]/@value')[0]
+    s.headers.update(
+        {'Content-type': 'application/x-www-form-urlencoded',
+            'Referer': HORIZON_URL})
+    payload = {'username': OS_USERNAME,
+               'password': OS_PASSWORD,
+               'csrfmiddlewaretoken': csrf_token,
+               'region': region}
+    try:
+        l = s.post(
+            ('%s:%s/auth/login/') % (HORIZON_URL, HORIZON_PORT),
+            data=payload,
+            verify=False)
+    except (exc.ConnectionError,
+            exc.HTTPError,
+            exc.Timeout) as e:
+        print 'status err While logging in: %s ' % e
+        sys.exit(1)
+
+    if not (l.ok and re.search('overview', l.content, re.IGNORECASE)):
+        print 'status err could not log in'
+        sys.exit(1)
+
+    login_status_code = l.status_code
+    login_seconds = l.elapsed.total_seconds()
+
+    print 'status OK'
     print 'metric splash_status_code uint32 %d' % splash_status_code
     print 'metric splash_seconds double %.2f' % splash_seconds
     print 'metric login_status_code uint32 %d' % login_status_code
