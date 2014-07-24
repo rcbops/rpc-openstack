e54878d43dc16c5d7ffd25b685efcb1291b5ec41
Ian Cordasco <ian.cordasco@rackspace.com>
2014-07-24 09:55:34 -0500

    Simplify monitoring script based on the assumption it will run inside the container

--- elasticsearch.py
+++ elasticsearch.py
@@ -1,40 +1,22 @@
+#!/usr/bin/env python
 import json
-import re
 import requests
-import subprocess
+
+ES_HOST = 'localhost'
+ES_PORT = '9200'
+ELASTICSEARCH = 'http://{0}:{1}'.format(ES_HOST, ES_PORT)
 
 
-def get_elasticsearch_container():
-    containers = subprocess.check_output('lxc-ls').strip().split('\n')
-    for container in containers:
-        if 'elasticsearch' in container:
-            break
-    else:
-        raise SystemExit(True)
-
-    return container
-
-
-def get_elasticsearch_address():
-    """Get the bind_host address from elasticsearch's config."""
-    es_container = get_elasticsearch_container()
-    cmd = ['lxc-attach', '-n', es_container, '--', 'cat',
-           '/etc/elasticsearch/elasticsearch.yml']
-    output = subprocess.check_output(cmd)
-    match = re.search('network\.bind_host:\s+([0-9\.]+)\s+', output)
-    return match.groups()[0]
-
-
-def find_indices(address):
+def find_indices():
     """Find indices created by logstash."""
-    url = 'http://{0}:9200/_search'.format(address)
+    url = ELASTICSEARCH + '/_search'
     r = requests.get(url, params={'_q': '_index like logstash%'})
     return sorted(res['_index'] for res in r.json()['hits']['hits'])
 
 
-def get_number_of(loglevel, address, index):
+def get_number_of(loglevel, index):
     """Retrieve the number of logs with level ``loglevel``."""
-    url = 'http://{0}:9200/{1}/_search'.format(address, index)
+    url = ELASTICSEARCH + '/' + index + '/_search'
     r = requests.get(url, data=json.dumps({
         'query': {
             'query_string': {
@@ -47,10 +29,9 @@ def get_number_of(loglevel, address, index):
 
 
 def main():
-    address = get_elasticsearch_address()
-    latest = find_indices(address)[-1]
-    num_errors = get_number_of('ERROR', address, latest)
-    num_warnings = get_number_of('WARN*', address, latest)
+    latest = find_indices()[-1]
+    num_errors = get_number_of('ERROR', latest)
+    num_warnings = get_number_of('WARN*', latest)
     print 'metric int NUMBER_OF_LOG_ERRORS {0}'.format(num_errors)
     print 'metric int NUMBER_OF_LOG_WARNINGS {0}'.format(num_warnings)
 
