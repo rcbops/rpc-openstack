d008d6a6c8a30747544b837b91a77cf6bc2e2c7f
Darren Birkett <darren.birkett@gmail.com>
2014-07-25 14:20:52 +0100

    remove garbage from lazy cut paste

--- neutron_api_global_check.py
+++ neutron_api_global_check.py
@@ -3,36 +3,6 @@
 import maas_common
 import sys
 
-NETWORK_ENDPOINT = 'http://127.0.0.1:9696'
-
-
-def check(token):
-
-    neutron = maas_common.get_neutron_client(token, NETWORK_ENDPOINT)
-
-    if neutron is None:
-        print 'status err Unable to obtain valid neutron client, ' \
-              'cannot proceed'
-        sys.exit(1)
-
-    print 'status OK'
-    print 'metric neutron_api_local_status uint32 1'
-
-
-def main():
-    auth_ref = maas_common.get_auth_ref()
-    token = auth_ref['token']['id']
-    check(token)
-
-
-if __name__ == "__main__":
-    main()
-root@aio1_neutron_server_container-c9b38c6d:/tmp# cat ./neutron_api_global_check.py
-#!/usr/bin/env python
-
-import maas_common
-import sys
-
 
 def check(auth_ref):
 
