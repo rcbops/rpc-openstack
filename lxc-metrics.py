99b4629b5b83c212b79ee84001aeefe2479449ad
Ian Cordasco <ian.cordasco@rackspace.com>
2014-07-29 16:12:01 -0500

    Use maas_common in lxc metrics

--- lxc-metrics.py
+++ lxc-metrics.py
@@ -3,6 +3,9 @@ import sys
 import subprocess
 import optparse
 
+from maas_common import status_err, status_ok, metric
+
+
 def retrieve_stats(path, metric, container=None):
     new_path = path + '/' + metric + '/lxc/'
     if container:
@@ -13,10 +16,9 @@ def retrieve_stats(path, metric, container=None):
     if metric == 'cpu':
         slab = new_path + '/cpu.stat'
 
-
     command = '/bin/cat ' + slab
     p = subprocess.Popen(command, stdout=subprocess.PIPE,
-                        stderr=subprocess.PIPE, shell=True)
+                         stderr=subprocess.PIPE, shell=True)
 
     stdout, stderr = p.communicate()
 
@@ -25,6 +27,7 @@ def retrieve_stats(path, metric, container=None):
 
     stdout = stdout.split('\n')
     return stdout
+
 
 def parse_metrics(metrics):
     del metrics[-1]
@@ -38,37 +41,34 @@ def parse_metrics(metrics):
 if __name__ == '__main__':
     parser = optparse.OptionParser()
     parser.add_option('--path', action='store',
-                     dest='lxc_cgroup_path',
-                     default='/sys/fs/cgroup',
-                     help="Path to lxc cgroup directories")
+                      dest='lxc_cgroup_path',
+                      default='/sys/fs/cgroup',
+                      help="Path to lxc cgroup directories")
     parser.add_option('--metric', action='store',
-                     dest='metric',
-                     help="Which metric to get (cpu, memory)")
+                      dest='metric',
+                      help="Which metric to get (cpu, memory)")
     parser.add_option('--system', action='store_true',
-                     dest='system',
-                     help="Retreive overall lxc cgroup metrics")
+                      dest='system',
+                      help="Retreive overall lxc cgroup metrics")
     parser.add_option('--container', action='store',
-                     dest='container',
-                     help='Get metrics by container <name>')
+                      dest='container',
+                      help='Get metrics by container <name>')
 
     (options, args) = parser.parse_args(sys.argv)
 
     cgroup_path = options.lxc_cgroup_path
-    metric = options.metric
     sys = options.system
     cont = options.container
 
-    if not metric:
-        print 'status err missing required argument: metric'
-        sys.exit(1)
+    if not options.metric:
+        status_err('missing required argument: metric')
 
     if not sys and not cont:
-        print 'status err neither system or container was defined'
-        sys.exit(1)
+        status_err('neither system or container was defined')
 
-    metrics = retrieve_stats(cgroup_path, metric, cont)
+    metrics = retrieve_stats(cgroup_path, options.metric, cont)
     metrics_list = parse_metrics(metrics)
 
-    print 'status OK'
-    for k,v in metrics_list.iteritems():
-        print 'metric %s int64 %s' % (k, v)
+    status_ok()
+    for k, v in metrics_list.iteritems():
+        metric(k, 'int64', v)
