b861c810dd5906a05008d7065a8bd8461fe462f8
Miguel Grinberg <miguelgrinberg50@gmail.com>
2014-07-29 11:50:12 -0700

    changed swift script to use maas_common helper functions and return time in ms

--- swift_api_global_check.py
+++ swift_api_global_check.py
@@ -3,15 +3,14 @@
 import sys
 from time import time
 from swiftclient import client
-import maas_common
+from maas_common import (status_ok, status_err, metric, get_auth_ref,
+                         get_keystone_client, get_neutron_client)
 
 
 def check(auth_ref):
-    keystone = maas_common.get_keystone_client(auth_ref)
+    keystone = get_keystone_client(auth_ref)
     if keystone is None:
-        print 'status err Unable to obtain valid keystone client, ' \
-              'cannot proceed'
-        sys.exit(1)
+        status_err('Unable to obtain valid keystone client, cannot proceed')
 
     endpoint = keystone.service_catalog.url_for(service_type='object-store',
                                                 endpoint_type='publicURL')
@@ -20,19 +19,19 @@ def check(auth_ref):
     swift = client.Connection(preauthurl=endpoint, preauthtoken=auth_token)
     start_time = time()
     account, containers = swift.get_account()
-    request_time = time() - start_time
+    request_time = int((time() - start_time) * 1000)
     total_objects = sum([c['count'] for c in containers])
     total_bytes = sum([c['bytes'] for c in containers])
 
-    print 'status OK'
-    print 'metric swift_response_time double %f' % request_time
-    print 'metric swift_containers uint32 %d' % len(containers)
-    print 'metric swift_objects uint64 %d' % total_objects
-    print 'metric swift_bytes uint64 %d' % total_bytes
+    status_ok()
+    metric('swift_response_time', 'uint32', request_time)
+    metric('swift_containers', 'uint32', len(containers))
+    metric('swift_objects', 'uint64', total_objects)
+    metric('swift_bytes', 'uint64', total_bytes)
 
 
 def main():
-    auth_ref = maas_common.get_auth_ref()
+    auth_ref = get_auth_ref()
     check(auth_ref)
 
 
