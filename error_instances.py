e82dddf0360f8fd694a886fd86ad93b682554515
Ian Cordasco <ian.cordasco@rackspace.com>
2014-07-29 15:55:23 -0500

    Use maas_common in error_instances.py

--- error_instances.py
+++ error_instances.py
@@ -19,15 +19,16 @@ import sys
 from nova import config
 from nova import context
 from nova.conductor import api
-from nova.openstack.common.rpc import impl_kombu, common
+from nova.openstack.common.rpc import common
+
+from maas_common import status, status_ok, metric
+
 
 def main(hosts):
     # not entirely sure what happens here, but once this is
     # run we have access to all the CONF keys/values
     config.parse_args([])
     conductor_api = api.API()
-    #ctxt = context.RequestContext(user_id="admin", project_id="admin",
-    #                              is_admin=True)
     ctxt = context.get_admin_context()
 
     # can't filter by {'host': None} for whatever reason :-/
@@ -39,7 +40,7 @@ def main(hosts):
     try:
         instances = conductor_api.instance_get_all_by_filters(ctxt, filters)
     except common.Timeout:
-        print "status timeout"
+        status('timeout')
         sys.exit(1)
 
     count = 0
@@ -52,9 +53,8 @@ def main(hosts):
 
         count += 1
 
-    print "status success"
-    print "metric error int32 %d" % count
-    #print "metric error int32 10"
+    status_ok()
+    metric('error', 'int32', count)
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
