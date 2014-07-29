afb3e2707ec7be5eddebf361793f6ba73c3715e1
Miguel Grinberg <miguelgrinberg50@gmail.com>
2014-07-29 11:40:41 -0700

    response time in ms, and fixed bad variable name

--- cinder_api_global_check.py
+++ cinder_api_global_check.py
@@ -12,16 +12,16 @@ def main():
 
     start_time = time()
     volumes = cinder.volumes.list()
-    request_time = time() - start_time
+    request_time = int((time() - start_time) * 1000)
     available = [v for v in volumes if v.status == 'available']
     errored = [v for v in volumes if 'error' in v.status]
 
     snaps = cinder.volume_snapshots.list()
-    snaps_available = [v for v in snapshots if v.status == 'available']
-    snaps_errored = [v for v in snapshots if 'error' in v.status]
+    snaps_available = [v for v in snaps if v.status == 'available']
+    snaps_errored = [v for v in snaps if 'error' in v.status]
 
     status_ok()
-    metric('cinder_response_time', 'double', request_time)
+    metric('cinder_response_time', 'uint32', request_time)
     metric('cinder_volumes', 'uint32', len(volumes))
     metric('cinder_volumes_available', 'uint32', len(available))
     metric('cinder_volumes_errored', 'uint32', len(errored))
