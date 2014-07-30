918c30a22bf6dc6e0c69037ba21759710c5e7602
Matt Thompson <mattt@defunct.ca>
2014-07-30 17:03:11 +0100

    Revert "Use new metric_bool function"
    This reverts commit e186b59c83f187900ce4bfd8b02b735d068434bb.

--- glance_registry_local_check.py
+++ glance_registry_local_check.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
 
-from maas_common import (status_ok, status_err, metric, metric_bool,
+from maas_common import (status_ok, status_err, metric, get_keystone_client,
                          get_auth_ref)
 from requests import Session
 from requests import exceptions as exc
 
 
 def check(auth_ref):
+    keystone = get_keystone_client(auth_ref)
+    tenant_id = keystone.tenant_id
+    auth_token = keystone.auth_token
     registry_endpoint = 'http://127.0.0.1:9191'
+
     api_status = 1
     milliseconds = 0
+
     s = Session()
 
     s.headers.update(
         {'Content-type': 'application/json',
-         'x-auth-token': auth_ref['token']['id']})
+         'x-auth-token': auth_token})
 
     try:
         # /images returns a list of public, non-deleted images
@@ -31,7 +36,7 @@ def check(auth_ref):
             api_status = 0
 
     status_ok()
-    metric_bool('glance_registry_local_status', api_status)
+    metric('glance_registry_local_status', 'uint32', api_status)
     metric('glance_registry_local_response_time', 'int32', milliseconds)
 
 
