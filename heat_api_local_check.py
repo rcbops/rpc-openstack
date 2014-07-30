23c468e3f2b3c68b173397b1c561d71db72f4006
Steve Lewis <stevelle@gmail.com>
2014-07-30 12:55:08 -0700

    conforming to evolving style, now returning status OK with a metric 'status' of 0 on service failure in the local check

--- heat_api_local_check.py
+++ heat_api_local_check.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python
 
-import requests
-from maas_common import (get_auth_ref, get_heat_client, get_keystone_client,
+from requests import Session
+from requests import exceptions as exc
+from maas_common import (get_auth_ref, get_keystone_client,
                          metric, status_ok, status_err)
 
 
 def check_heat(tenant, token):
-    endpoint = 'http://127.0.0.1:8004/v1/{0}'.format(tenant)
-    get_heat_client(endpoint, token)
+    s = Session()
+    s.headers.update(
+        {'Content-type': 'application/json',
+         'x-auth-token': token})
+
+    api_status = 1
+    try:
+        r = s.get('http://127.0.0.1:8004/v1/{0}/build_info'.format(tenant), verify=False, timeout=10)
+    except (exc.ConnectionError, exc.HTTPError, exc.Timeout):
+        api_status = 0
+        milliseconds = -1
+    except Exception as e:
+        status_err(str(e))
+    else:
+        milliseconds = r.elapsed.total_seconds() * 1000.0
+        if not r.ok:
+            api_status = 0
 
     status_ok()
-    metric('heat_api_local_status', 'uint32', 1)
+    metric('heat_api_local_status', 'uint32', api_status)
+    metric('heat_local_response_time', 'int32', milliseconds)
 
 
 def main():
