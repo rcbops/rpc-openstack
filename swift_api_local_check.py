2d7f12efbc912f7d726a02a5dcd4f333e5dca9e4
Miguel Grinberg <miguelgrinberg50@gmail.com>
2014-08-01 11:51:40 -0700

    keystone errors now cause an error status

--- swift_api_local_check.py
+++ swift_api_local_check.py
@@ -14,41 +14,38 @@ except Exception as e:
 
 def check(auth_ref):
     status_msg = None
-    api_ok = True
+    api_is_up = True
     elapsed = -1
 
+    keystone = get_keystone_client(auth_ref)
+    if keystone is None:
+        status_err('Unable to obtain valid keystone client')
+
+    endpoint = keystone.service_catalog.url_for(
+        service_type='object-store', endpoint_type='publicURL')
+    auth_token = keystone.auth_ref['token']['id']
+
     try:
-        keystone = get_keystone_client(auth_ref)
-        if keystone is None:
-            status_msg = 'Unable to obtain valid keystone client'
-            api_ok = False
+        swift = client.Connection(preauthurl=endpoint, preauthtoken=auth_token)
+        if swift is None:
+            status_msg = 'Unable to obtain valid swift client'
+            api_is_up = False
         else:
-            endpoint = keystone.service_catalog.url_for(
-                service_type='object-store', endpoint_type='publicURL')
-            auth_token = keystone.auth_ref['token']['id']
+            # make a simple call to make sure everything works
+            start = time()
+            swift.get_account()
+            elapsed = int((time() - start) * 1000)
 
-            swift = client.Connection(preauthurl=endpoint,
-                                      preauthtoken=auth_token)
-            if swift is None:
-                status_msg = 'Unable to obtain valid swift client'
-                api_ok = False
-            else:
-                # make a simple call to make sure everything works
-                try:
-                    start = time()
-                    swift.get_account()
-                    elapsed = int((time() - start) * 1000)
-
-                except ClientException:
-                    status_msg = 'Unable to connect to swift service'
-                    api_ok = False
+    except ClientException as e:
+        status_msg = str(e)
+        api_is_up = False
 
     except Exception as e:
-        # unknown expection, exit with error
+        # unknown exception, exit with error
         status_err(str(e))
 
     status_ok(status_msg)
-    metric_bool('swift_api_local_status', api_ok)
+    metric_bool('swift_api_local_status', api_is_up)
     metric('swift_api_response_time', 'int32', elapsed)
 
 
