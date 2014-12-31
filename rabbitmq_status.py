4a2f32d17ce141243939d940623dd02eab65add8
BjoernT <bjoern@teipel.ws>
2014-12-31 08:55:40 -0600

    Simplifying channel check for #138

--- rabbitmq_status.py
+++ rabbitmq_status.py
@@ -90,9 +90,8 @@ def main():
 
     if r.ok:
         resp_json = r.json()  # Parse the JSON once
-        for i in range(len(resp_json)):
-            if resp_json[i]['number'] > 1:
-               status_err('Detected RabbitMQ connections with multiple channels. Please check RabbitMQ and all Openstack consumers')
+        if any(channel['number'] > 1 for channel in resp_json):
+           status_err('Detected RabbitMQ connections with multiple channels. Please check RabbitMQ and all Openstack consumers')
     else:
         status_err('Received status {0} from RabbitMQ API'.format(
             r.status_code))
