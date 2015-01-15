838631ca6f702874b324b8e67c257162b41ab037
Miguel Grinberg <miguelgrinberg50@gmail.com>
2015-01-14 16:34:28 -0800

    added support for parameter types

--- rackspace/heat_store/catalog/solution.py
+++ rackspace/heat_store/catalog/solution.py
@@ -10,7 +10,7 @@ from markdown.inlinepatterns import ImagePattern, ImageReferencePattern, \
     IMAGE_LINK_RE, IMAGE_REFERENCE_RE
 try:
     from openstack_dashboard import api
-except ImportError:
+except:
     api = None
 
 
@@ -51,28 +51,18 @@ class Solution(object):
     """
     def __init__(self, info_yaml, basedir=''):
         """Import the solution's info.yaml file."""
-        url_parts = urlparse(info_yaml)
-
-        # read yaml content
-        if url_parts.scheme == '':
-            if not os.path.isabs(info_yaml):
-                info_yaml = os.path.join(basedir, info_yaml)
-                url_parts = urlparse(info_yaml)
-            f = open(info_yaml, 'rt')
-        else:
-            f = urlrequest.urlopen(info_yaml)
+        f, url_parts = self._open(info_yaml, basedir)
         solution_yaml = f.read().decode('utf-8')
-
-        self.base_url = urlunparse((url_parts.scheme, url_parts.netloc,
+        self.basedir = urlunparse((url_parts.scheme, url_parts.netloc,
                                    os.path.dirname(url_parts.path),
                                    None, None, None))
 
         # create a markdown converter and modify it to rebase image links
         markdown = Markdown()
         markdown.inlinePatterns['image_link'] = _RebasedImageLinkPattern(
-            self.base_url, IMAGE_LINK_RE, markdown)
+            self.basedir, IMAGE_LINK_RE, markdown)
         markdown.inlinePatterns['image_reference'] = _RebasedImageRefPattern(
-            self.base_url, IMAGE_REFERENCE_RE, markdown)
+            self.basedir, IMAGE_REFERENCE_RE, markdown)
 
         # import the solution's metadata
         info = yaml.load(solution_yaml)
@@ -86,22 +76,136 @@ class Solution(object):
         self.heat_template = info['heat_template']
         self.env_file = info.get('env_file')  # environments are optional
         desc_url = info['long_description']
-        url_parts = urlparse(desc_url)
-        if url_parts.scheme == '' and not os.path.isabs(url_parts.path):
-            desc_url = self._relative_to_absolute(info['long_description'])
-            url_parts = urlparse(desc_url)
-        if url_parts.scheme == '':
-            f = open(desc_url)
-        else:
-            f = urlrequest.urlopen(desc_url)
+
+        # read the markdown description
+        f, url_parts = self._open(desc_url, self.basedir)
         self.long_description = markdown.convert(f.read().decode('utf-8'))
 
-    def _relative_to_absolute(self, rel):
-        """Convert a filename relative to the solution's URL to absolute."""
-        url_parts = urlparse(rel)
-        if url_parts.scheme != '' or os.path.isabs(url_parts.path):
-            return rel  # already absolute
-        return os.path.join(self.base_url, rel)
+        # initialize parameters
+        self.parameter_types = []
+
+    def get_parameter_types(self, request):
+        """Return the parameter list for this solution."""
+        if self.parameter_types:
+            return self.parameter_types
+
+        # import heat template to obtain its parameters
+        f, url_parts = self._open(self.heat_template, self.basedir)
+        template = yaml.load(f.read().decode('utf-8'))
+        params_in_order = []
+        if template.get('parameter_groups'):
+            for pgroup in template.get('parameter_groups'):
+                for param in pgroup.get('parameters', []):
+                    params_in_order.append(param)
+        params = template.get('parameters')
+        if not params_in_order:
+            params_in_order = params.keys()
+        if params_in_order:
+            for name in params_in_order:
+                param = params[name]
+
+                # there are a few commonly used string parameters such as
+                # flavors, images, keynames, etc. that we want to present as
+                # dropdowns, so we change them to a more appropriate type here
+                param_type = param['type']
+                param_constraints = param.get('constraints', [])
+                param_default = param.get('default')
+                param_mapping = None
+                if param_type == 'string' and param_constraints == []:
+                    if name.endswith('flavor'):
+                        flavors = api.nova.flavor_list(request)
+                        param_type = 'comma_delimited_list'
+                        param_constraints = [
+                            {'allowed_values': [flavor.name
+                                                for flavor in flavors]}
+                        ]
+                        if flavors and not param_default:
+                            param_default = flavors[0].name
+                    elif name.endswith('image'):
+                        images, more, prev = \
+                            api.glance.image_list_detailed(request)
+                        param_type = 'comma_delimited_list'
+                        param_constraints = [
+                            {'allowed_values': [image.name
+                                                for image in images]}
+                        ]
+                        if images and not param_default:
+                            param_default = images[0].name
+                    elif name.endswith('keyname'):
+                        keypairs = api.nova.keypair_list(request)
+                        param_type = 'comma_delimited_list'
+                        param_constraints = [
+                            {'allowed_values': [keypair.name
+                                                for keypair in keypairs]}
+                        ]
+                        if keypairs and not param_default:
+                            param_default = keypairs[0].name
+                    elif name == 'floating-network-id':
+                        networks = api.neutron.network_list(
+                            request, **{'router:external': True})
+                        param_type = 'comma_delimited_list'
+                        param_constraints = [
+                            {'allowed_values': [network.name
+                                                for network in networks]}
+                        ]
+                        if networks and not param_default:
+                            param_default = networks[0].name
+                        param_mapping = {}
+                        for network in networks:
+                            param_mapping[network.name] = network.id
+
+                p = {
+                    'name': name,
+                    'type': param_type,
+                    'constraints': param_constraints,
+                    'label': param['label'],
+                    'description': param.get('description'),
+                    'default': param_default
+                }
+                if param_mapping:
+                    p['_mapping'] = param_mapping
+                self.parameter_types.append(p)
+        return self.parameter_types
+
+    def map_parameter(self, param, value):
+        """Map the value provided by the user to the value needed by Heat."""
+        if '_mapping' not in param:
+            return value
+        return param['_mapping'].get(value, value)
+
+    def launch(self, request, params={}):
+        """Launch the solution's heat template."""
+        if not api or not api.heat:
+            raise RuntimeError('Heat API is not available.')
+
+        fields = {
+            'stack_name': (re.sub('[\W\d]+', '_', self.title.strip()) +
+                           '_' + str(uuid.uuid4())),
+            'timeout_mins': 60,
+            'disable_rollback': False,
+            'parameters': params,
+            'template_url': self.heat_template,
+            'environment': self._get_environment_data()  # can't use URL here
+        }
+        api.heat.stack_create(request, **fields)
+
+    def _open(self, file_or_url, basedir=''):
+        """Open a local or remote file.
+
+        :param file_or_url: The filename or remote URL to open.
+        :param basedir: The path or URL to prepend when `file_or_url` is
+                        relative.
+        :returns a tuple with the file handle and the urlparsed filename.
+        """
+        url_parts = urlparse(file_or_url)
+        if url_parts.scheme == '' and not os.path.isabs(url_parts.path):
+            file_or_url = os.path.join(basedir, file_or_url)
+            url_parts = urlparse(file_or_url)
+        if url_parts.scheme == '':
+            f = open(file_or_url)
+        else:
+            f = urlrequest.urlopen(file_or_url)
+        return f, url_parts
 
     def _get_environment_data(self):
         """Return the contents of the heat environment file (if provided).
@@ -113,23 +217,6 @@ class Solution(object):
             return None
         f = urlrequest.urlopen(self.env_file)
         return f.read().decode('utf-8')
-
-    def launch(self, request):
-        """Launch the solution's heat template."""
-        if not api or not api.heat:
-            raise RuntimeError('Heat API is not available.')
-
-        fields = {
-            'stack_name': (re.sub('[\W\d]+', '_', self.title.strip()) +
-                           '_' + str(uuid.uuid4())),
-            'timeout_mins': 60,
-            'disable_rollback': False,
-            'parameters': {},
-            # 'password': '',
-            'template_url': self.heat_template,
-            'environment': self._get_environment_data()  # can't use URL here
-        }
-        api.heat.stack_create(request, **fields)
 
 
 class Catalog(object):
--- rackspace/heat_store/catalog/tests.py
+++ rackspace/heat_store/catalog/tests.py
@@ -18,27 +18,23 @@ class TestSolution(unittest.TestCase):
         self.mox.UnsetStubs()
 
     def test_create_solution(self):
-        s = Solution('test_data/info.yaml')
-        self.assertEqual(s.id, 'b13b31472e42e8ca55f51cb7bd89574a')
+        s = Solution('test_data/example/info.yaml')
+        self.assertEqual(s.id, 'ffeb917af8e0fb0bf90c4d11d5b0fe0a')
         self.assertEqual(s.title, 'the_title')
         self.assertEqual(s.short_description, 'the short description')
         self.assertIn('the <em>long</em> description', s.long_description)
-        self.assertIn('src="test_data/diagram.png"', s.long_description)
+        self.assertIn('src="test_data/example/diagram.png"', s.long_description)
         self.assertIn('alt="here is a diagram"', s.long_description)
         self.assertIn('highlight_1', s.highlights)
         self.assertIn('highlight_2', s.highlights)
         self.assertIn({'example.com': 'http://example.com/'}, s.links)
         self.assertIn({'Download': 'http://download.example.com/'}, s.links)
-        self.assertEqual(s.heat_template,
-                         'https://raw.githubusercontent.com/' +
-                         'example/heat-example/master/example.yaml')
-        self.assertEqual(s.env_file,
-                         'https://raw.githubusercontent.com/' +
-                         'example/heat-example/master/env.yaml')
+        self.assertEqual(s.heat_template, 'example.yaml')
+        self.assertEqual(s.env_file, 'env.yaml')
         self.assertEqual(s.template_version, '0.0.1')
 
     def test_incomplete_solution(self):
-        lines = open('test_data/info.yaml').readlines()
+        lines = open('test_data/example/info.yaml').readlines()
 
         # ensure the solution isn't imported if any of the items
         # below are missing
@@ -56,6 +52,39 @@ class TestSolution(unittest.TestCase):
         for missing in missing_list:
             with self.assertRaises(KeyError):
                 Solution('http://example.com/no-{0}.yaml'.format(missing))
+
+    def test_parameter_types(self):
+        s = Solution('test_data/example/info.yaml')
+        params = s.get_parameter_types(None)
+
+        self.assertEqual(len(params), 5)
+
+        self.assertEqual(params[0]['name'], 'floating-network-id')
+        self.assertEqual(params[0]['type'], 'comma_delimited_list')
+        self.assertIn(params[0]['default'],
+                      params[0]['constraints'][0]['allowed_values'])
+        self.assertIn('_mapping', params[0])
+        self.assertEqual(params[0]['_mapping'], {'first_network': '11',
+                                                 'second_network': '22',
+                                                 'third_network': '33'})
+        self.assertEqual(s.map_parameter(params[0], 'second_network'), '33')
+
+        self.assertEqual(params[1]['name'], 'flavor')
+        self.assertEqual(params[1]['type'], 'comma_delimited_list')
+        self.assertIn(params[1]['default'], 'm1.small')
+
+        self.assertEqual(params[2]['name'], 'image')
+        self.assertEqual(params[2]['type'], 'comma_delimited_list')
+        self.assertIn(params[2]['default'],
+                      params[2]['constraints'][0]['allowed_values'])
+
+        self.assertEqual(params[3]['name'], 'image-count')
+        self.assertEqual(params[3]['type'], 'number')
+
+        self.assertEqual(params[4]['name'], 'keyname')
+        self.assertEqual(params[4]['type'], 'comma_delimited_list')
+        self.assertIn(params[4]['default'],
+                      params[4]['constraints'][0]['allowed_values'])
 
 
 if __name__ == '__main__':
