7b3f092ba6a3ab920d4e448a87657488e2f817ff
Miguel Grinberg <miguelgrinberg50@gmail.com>
2015-02-01 10:38:13 -0800

    removed hardcoded dependency to os-ansible-deployment repo

--- rpc_deployment/inventory/dynamic_inventory.py
+++ rpc_deployment/inventory/dynamic_inventory.py
@@ -2,9 +2,12 @@
 import os
 import sys
 import subprocess
+import yaml
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-rpc_inventory = os.path.join(
-    basedir, ('../../../os-ansible-deployment/rpc_deployment/inventory/'
-              'dynamic_inventory.py'))
+vars_yaml = os.path.join(basedir, '../vars/common.yml')
+rpc_repo_path = yaml.load(open(vars_yaml, 'r'))['rpc_repo_path']
+
+rpc_inventory = os.path.join(rpc_repo_path,
+                             'rpc_deployment/inventory/dynamic_inventory.py')
 subprocess.call([rpc_inventory] + sys.argv[1:])
