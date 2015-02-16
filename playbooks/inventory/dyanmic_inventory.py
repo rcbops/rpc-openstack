4059eb634777b671aae280fb801193e78c0f3b36
Andy McCrae <andy.mccrae@gmail.com>
2015-02-16 16:40:35 +0000

    Move MaaS to rpc-extras
    Set MaaS up as a galaxy-role
    Move all the bits to the rpc-extras repo
    Adjust the rackspace-monitoring-cli pip package to be downloaded from
    pypi
    Utilise the existing os-ansible-deployment repo's keystone library
    Utilise the existing os-ansible-deployment repo's dynamic_inventory

---
+++ playbooks/inventory/dyanmic_inventory.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import os
+import sys
+import subprocess
+import yaml
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+vars_yaml = os.path.join(basedir, '/etc/openstack_deploy/user_maas_settings.yml')
+rpc_repo_path = yaml.load(open(vars_yaml, 'r'))['rpc_repo_path']
+
+rpc_inventory = os.path.join(rpc_repo_path,
+                             'playbooks/inventory/dynamic_inventory.py')
+subprocess.call([rpc_inventory] + sys.argv[1:])
