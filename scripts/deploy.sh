3b7dd22ba1de0da355b9e64910dd9925fb8a0c85
Matt Thompson <mattt@defunct.ca>
2015-07-28 12:49:09 +0100

    Add Ceph support
    This commit adds support for deploying a Ceph cluster using
    rpc-openstack as well setting the appropriate configuration for
    integration with os-ansible-deployment's Ceph capabilities.
    
    This commit makes use of the following roles to deploy a Ceph cluster:
    
    https://github.com/ceph/ansible-ceph-common
    https://github.com/ceph/ansible-ceph-mon
    https://github.com/ceph/ansible-ceph-osd
    
    The roles are submodules and so should get automatically cloned at the
    same time as os-ansible-deployment.
    
    The default configuration is designed to deploy three mon containers,
    one on each controller, with separate physical hosts for OSDs.
    
    The Ceph pools and users required by OpenStack are automatically created
    as part of the setup.
    
    Ceph can be deployed on a AIO, when this is done three OSD containers
    are created in an attempt to provide a more realistic representation of
    a Ceph cluster.
    
    The rbd pool is created automatically when a new Ceph cluster is
    deployed. This pool is not used by an OpenStack deployment but does
    consume pgs and so is removed if it exists and is empty.
    
    This commit sets the following in user_extras_variables.yml:
    
    pool_default_size: 3
    pool_default_min_size: 2
    mon_osd_full_ratio: .90
    mon_osd_nearfull_ratio: .80
    raw_multi_journal: true
    journal_size: 80000
    secure_cluster: true
    secure_cluster_flags:
      - nodelete
    
    The role defaults are:
    
    pool_default_size: 2
    pool_default_min_size: 1
    mon_osd_full_ratio: .95
    mon_osd_nearfull_ratio: .85
    raw_multi_journal: false
    journal_size: 0
    secure_cluster: false
    secure_cluster_flags:
      - nopgchange
      - nodelete
      - nosizechange
    
    The Ceph playbooks have been excluded from ansible-lint because their
    inclusion causes the third-party Ceph roles to be tested and they fail
    the ansible-lint checks.
    
    Co-Authored-By: git-harry <git-harry@live.co.uk>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,11 +11,14 @@ export DEPLOY_ELK=${DEPLOY_ELK:-"yes"}
 export DEPLOY_MAAS=${DEPLOY_MAAS:-"no"}
 export DEPLOY_TEMPEST=${DEPLOY_TEMPEST:-"no"}
 export DEPLOY_CEILOMETER=${DEPLOY_CEILOMETER:-"no"}
+export DEPLOY_CEPH=${DEPLOY_CEPH:-"no"}
 export FORKS=${FORKS:-$(grep -c ^processor /proc/cpuinfo)}
 
 source /opt/rpc-openstack/openstack-ansible/scripts/scripts-library.sh
 OA_DIR='/opt/rpc-openstack/openstack-ansible'
 RPCD_DIR='/opt/rpc-openstack/rpcd'
+RPCD_VARS='/etc/openstack_deploy/user_extras_variables.yml'
+RPCD_SECRETS='/etc/openstack_deploy/user_extras_secrets.yml'
 
 # begin the bootstrap process
 cd ${OA_DIR}
@@ -32,13 +35,32 @@ if [[ "${DEPLOY_AIO}" == "yes" ]]; then
         for i in $(find etc/openstack_deploy/ -type f -iname '*.yml'); do ../scripts/update-yaml.py /$i $i; done
     popd
     # ensure that the elasticsearch JVM heap size is limited
-    sed -i 's/# elasticsearch_heap_size_mb/elasticsearch_heap_size_mb/' /etc/openstack_deploy/user_extras_variables.yml
+    sed -i 's/# elasticsearch_heap_size_mb/elasticsearch_heap_size_mb/' $RPCD_VARS
     # set the kibana admin password
-    sed -i "s/kibana_password:.*/kibana_password: ${ADMIN_PASSWORD}/" /etc/openstack_deploy/user_extras_secrets.yml
+    sed -i "s/kibana_password:.*/kibana_password: ${ADMIN_PASSWORD}/" $RPCD_SECRETS
     # set the load balancer name to the host's name
-    sed -i "s/lb_name: .*/lb_name: '$(hostname)'/" /etc/openstack_deploy/user_extras_variables.yml
+    sed -i "s/lb_name: .*/lb_name: '$(hostname)'/" $RPCD_VARS
     # set the notification_plan to the default for Rackspace Cloud Servers
-    sed -i "s/maas_notification_plan: .*/maas_notification_plan: npTechnicalContactsEmail/" /etc/openstack_deploy/user_extras_variables.yml
+    sed -i "s/maas_notification_plan: .*/maas_notification_plan: npTechnicalContactsEmail/" $RPCD_VARS
+    # set the necessary bits for ceph
+    if [[ "$DEPLOY_CEPH" == "yes" ]]; then
+      cp -a ${RPCD_DIR}/etc/openstack_deploy/conf.d/ceph.yml.aio /etc/openstack_deploy/conf.d/ceph.yml
+
+      # In production, the OSDs will run on bare metal however in the AIO we'll put them in containers
+      # so the MONs think we have 3 OSDs on different hosts.
+      sed -i 's/is_metal: true/is_metal: false/' /etc/openstack_deploy/env.d/ceph.yml
+
+      sed -i "s/journal_size:.*/journal_size: 1024/" $RPCD_VARS
+      echo "monitor_interface: eth1" | tee -a $RPCD_VARS
+      echo "public_network: 172.29.236.0/22" | tee -a $RPCD_VARS
+      sed -i "s/raw_multi_journal:.*/raw_multi_journal: false/" $RPCD_VARS
+      echo "osd_directory: true" | tee -a $RPCD_VARS
+      echo "osd_directories:" | tee -a $RPCD_VARS
+      echo "  - /var/lib/ceph/osd/mydir1" | tee -a $RPCD_VARS
+      sed -i "s/glance_default_store:.*/glance_default_store: rbd/" /etc/openstack_deploy/user_variables.yml
+      echo "nova_libvirt_images_rbd_pool: vms" | tee -a /etc/openstack_deploy/user_variables.yml
+      echo "cinder_ceph_client_uuid:"  | tee -a /etc/openstack_deploy/user_secrets.yml
+    fi
     # set the ansible inventory hostname to the host's name
     sed -i "s/aio1/$(hostname)/" /etc/openstack_deploy/openstack_user_config.yml
     sed -i "s/aio1/$(hostname)/" /etc/openstack_deploy/conf.d/*.yml
@@ -49,7 +71,8 @@ fi
 which openstack-ansible || ./scripts/bootstrap-ansible.sh
 
 # ensure all needed passwords and tokens are generated
-./scripts/pw-token-gen.py --file /etc/openstack_deploy/user_extras_secrets.yml
+./scripts/pw-token-gen.py --file /etc/openstack_deploy/user_secrets.yml
+./scripts/pw-token-gen.py --file $RPCD_SECRETS
 
 # Apply any patched files.
 cd ${RPCD_DIR}/playbooks
@@ -72,6 +95,12 @@ if [[ "${DEPLOY_OA}" == "yes" ]]; then
 
   # setup the hosts and build the basic containers
   install_bits setup-hosts.yml
+
+  if [[ "$DEPLOY_CEPH" == "yes" ]]; then
+    pushd ${RPCD_DIR}/playbooks/
+      install_bits ceph-all.yml
+    popd
+  fi
 
   # setup the infrastructure
   install_bits setup-infrastructure.yml
