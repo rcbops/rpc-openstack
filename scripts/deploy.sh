rcbops/rpc-openstack

commit 37cc4fbda1551b71b572037f4b6519fc2b512d77

Check for files in /etc/openstack_deploy and copy if missing
Before running update_yaml.py we check to see if the file exists
in /etc/openstack_deploy.  If not we copy the file from the skeleton
directory.

Fixes-Issue: #576

Updated iterator name to filename, and quoted variables

Revert "Updated iterator name to filename, and quoted variables"

This reverts commit 577784c8149415064666f6666eea5c915e86d163.

Updated iterator name to filename

Quoted filename variable

Best Practices Quoting

Check for files in /etc/openstack_deploy and copy if missing

Before running update_yaml.py we check to see if the file exists
in /etc/openstack_deploy.  If not we copy the file from the skeleton
directory.

Fixes-Issue: #576

Updated iterator name to filename, and quoted variables

Revert "Updated iterator name to filename, and quoted variables"

This reverts commit 577784c8149415064666f6666eea5c915e86d163.

Updated iterator name to filename

Quoted filename variable

Best practice quoting

Move slashes inside the quotes

scripts/deploy.sh
@@ -36,7 +36,12 @@ if [[ "${DEPLOY_AIO}" == "yes" ]]; then
   if [[ ! -d /etc/openstack_deploy/ ]]; then
     ./scripts/bootstrap-aio.sh
     pushd ${RPCD_DIR}
-        for i in $(find etc/openstack_deploy/ -type f -iname '*.yml'); do ../scripts/update-yaml.py /$i $i; done
+      for filename in $(find etc/openstack_deploy/ -type f -iname '*.yml');
+        if [[ ! -a "/${filename}" ]]; then
+          cp "${filename}" "/${filename}";
+        fi
+        do ../scripts/update-yaml.py "/${filename}" "${filename}";
+      done
     popd
     # ensure that the elasticsearch JVM heap size is limited
     sed -i 's/# elasticsearch_heap_size_mb/elasticsearch_heap_size_mb/' $RPCD_VARS

commit 9ffc160dfddbbf22c0943f4ad55a769554a89697

Fix for loop syntax for updating Ansible config
37cc4fbda1551b71b572037f4b6519fc2b512d77 has introduced a bug because
the for-loop syntax is incorrect. This patch moves the 'do' so that the
loop will run.

Closes-issue: https://github.com/rcbops/rpc-openstack/issues/583

scripts/deploy.sh
@@ -36,11 +36,11 @@ if [[ "${DEPLOY_AIO}" == "yes" ]]; then
   if [[ ! -d /etc/openstack_deploy/ ]]; then
     ./scripts/bootstrap-aio.sh
     pushd ${RPCD_DIR}
-      for filename in $(find etc/openstack_deploy/ -type f -iname '*.yml');
+      for filename in $(find etc/openstack_deploy/ -type f -iname '*.yml'); do
         if [[ ! -a "/${filename}" ]]; then
           cp "${filename}" "/${filename}";
         fi
-        do ../scripts/update-yaml.py "/${filename}" "${filename}";
+        ../scripts/update-yaml.py "/${filename}" "${filename}";
       done
     popd
     # ensure that the elasticsearch JVM heap size is limited