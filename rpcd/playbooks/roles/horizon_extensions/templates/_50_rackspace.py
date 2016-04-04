b38f5848ca1c85f22c7cb9861355c3a1378ff4dd
Richard Jones <r1chardj0n3s@gmail.com>
2016-04-04 15:55:18 +1000

    Include the static files in the extension
    fixes #938
    
    (cherry picked from commit 4e67b76b6f1a04f7bfeff2b0fff34c7e445371e9)
    Signed-off-by: Matthew Thode <mthode@mthode.org>

--- rpcd/playbooks/roles/horizon_extensions/templates/_50_rackspace.py
+++ rpcd/playbooks/roles/horizon_extensions/templates/_50_rackspace.py
@@ -6,5 +6,9 @@ ADD_INSTALLED_APPS = [
 
 ADD_ANGULAR_MODULES = ['horizon.dashboard.rackspace']
 
+# Have Horizon find all the static files here when adding links to
+# the base page template.
+AUTO_DISCOVER_STATIC_FILES = True
+
 # If set to True, this dashboard will not be added to the settings.
 DISABLED = False
