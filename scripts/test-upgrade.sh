860d615144a6a62fe4878deed826af7c49b0da2b
Matthew Thode <mthode@mthode.org>
2016-08-23 10:57:31 -0500

    add migrate-yaml to the test-upgrade script
    Connects https://github.com/rcbops/u-suk-dev/issues/376

--- scripts/test-upgrade.sh
+++ scripts/test-upgrade.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright 2014, Rackspace US, Inc.
+# Copyright 2014-2016, Rackspace US, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,12 +14,27 @@
 # limitations under the License.
 #
 # (c) 2015, Nolan Brubaker <nolan.brubaker@rackspace.com>
+# (c) 2016, Matthew Thode <matt.thode@rackspace.com>
 
 set -eux -o pipefail
 
 BASE_DIR=$( cd "$( dirname ${0} )" && cd ../ && pwd )
 OA_DIR="$BASE_DIR/openstack-ansible"
 RPCD_DIR="$BASE_DIR/rpcd"
+
+# TASK #0
+# Bug: https://github.com/rcbops/u-suk-dev/issues/347
+# Issue: Variables files have been reorganized, run a migration script to move
+#        them to the new location.
+"${BASE_DIR}"/scripts/migrate-yaml.py --for-testing-take-new-vars-only \
+    --defaults "${RPCD_DIR}"/etc/openstack_deploy/user_rpco_variables_defaults.yml \
+    --overrides /etc/openstack_deploy/user_extra_variables.yml \
+    --output-file /etc/openstack_deploy/user_rpco_variables_overrides.yml
+"${BASE_DIR}"/scripts/migrate-yaml.py --for-testing-take-new-vars-only \
+  --defaults "${RPCD_DUR}"/etc/openstack_deploy/user_osa_variables_defaults.yml \
+  --overrides /etc/openstack_deploy/user_variables.yml \
+  --output-file /etc/openstack_deploy/user_osa_variables_overrides.yml
+rm /etc/openstack_deploy/user_extra_variables.yml /etc/openstack_deploy/user_variables.yml
 
 # TASK #1
 # Bug: https://github.com/rcbops/rpc-openstack/issues/1345
