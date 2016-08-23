c56a911e0150948423256f2d8b5860544a7064ae
Matthew Thode <mthode@mthode.org>
2016-08-23 16:04:35 -0500

    Simplify migrate-yaml.py
    Write a file containing new overrides with the new defaults stripped
    from the old overrides and conflicts listed in their own keys.

--- scripts/migrate-yaml.py
+++ scripts/migrate-yaml.py
@@ -16,24 +16,13 @@
 # (c) 2016, Matthew Thode <matt.thode@rackspace.com>
 
 """
-Print out values that have changed, by default just additions and changes.
-
-Additions can be taken as is and placed into an overrides file, but should
-you should verify you still want to keep it.
-
-Removals are not printed by default because not having an override is the
-same as taking a default.
-
-Changes are printed, showing what you overrode and what is getting overridden.
-
-CAUTION: danger-mode is only if you KNOW what you are overriding doesn't
-    need to change, warrantee is void if used.
+Write a file containing new overrides with the new defaults stripped from the
+old overrides and conflicts listed in their own keys.
 """
 
 from __future__ import print_function
 import argparse
-from collections import namedtuple
-from deepdiff import DeepDiff
+import os
 import yaml
 
 
@@ -41,28 +30,11 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--overrides', dest='overrides', required=True,
                         help='The path to the file with overrides.')
-    parser.add_argument('--info', dest='info', type=bool, default=False,
-                        help='Shows what is NOT overridden.')
-    parser.add_argument('--danger-mode', dest='danger',
-                        type=bool, default=False,
-                        help='Print output usable as an overrides file.')
+    parser.add_argument('--defaults', dest='defaults', required=True,
+                        help='The path to the file with defaults.')
+    parser.add_argument('--output-file', dest='output_file', required=True,
+                        help='The path to the new overrides file location')
     return parser.parse_args()
-
-
-def root_cleanup(var):
-    """Cleans up what deepdiff returns so we can have some cleaner output.
-
-    :param var: the key you wish to clean up, just a string
-    :return: the cleaned up string
-    """
-    prefix = "root['"
-    suffix = "']"
-    if var.startswith(prefix):
-        var = var[len(prefix):]
-    if var.endswith(suffix):
-        var = var[:-len(suffix)]
-    var = var.replace("']['", '.')
-    return var
 
 
 def parsed_yaml_from(path):
@@ -81,8 +53,8 @@ def parsed_yaml_from(path):
     else:
         try:
             loaded_data = yaml.safe_load(data)
-        except yaml.ParserError:
-            raise SystemExit('The yaml in %s is non-compliant and failed'
+        except yaml.parser.ParserError:
+            raise SystemExit('The yaml in %s is non-compliant and failed '
                              'to load' % path)
 
     if loaded_data is None:
@@ -96,97 +68,57 @@ def do_the_diff(defaults, overrides):
 
     :param defaults: File you wish to compare against, the base.
     :param overrides: File you wish to compare, the mask.
-    :return: what is added, taken away, changed(added) and changed(removed).
-    :rtype: namedtuple
+    :return: stuff
+    :rtype: dict
     """
-    added = {}
-    removed = {}
-    changed = {}
-    changed_types = {}
-    parsed_diff = namedtuple('parsed_yaml_diff',
-                             ['added', 'removed',
-                              'changed_added', 'changed_removed'])
 
-    diff = DeepDiff(defaults, overrides, verbose_level=2)
+    new_combined_diff = {}
 
-    for old_key, value in diff.get('dictionary_item_added', {}).items():
-        added[root_cleanup(old_key)] = value
-    for old_key, value in diff.get('dictionary_item_removed', {}).items():
-        removed[root_cleanup(old_key)] = value
-    for old_key, value in diff.get('values_changed', {}).items():
-        changed[root_cleanup(old_key)] = value
-    for old_key, value in diff.get('type_changes', {}).items():
-        changed_types[root_cleanup(old_key)] = value
+    for key in overrides.keys():
+        if key not in defaults.keys():
+            new_combined_diff[key] = overrides[key]
+        else:
+            if defaults[key] == overrides[key]:
+                pass
+            else:
+                if 'NEW_DEFAULTS' not in new_combined_diff.keys():
+                    new_combined_diff['NEW_DEFAULTS'] = {}
+                if 'OLD_OVERRIDES' not in new_combined_diff.keys():
+                    new_combined_diff['OLD_OVERRIDES'] = {}
+                new_combined_diff['NEW_DEFAULTS'][key] = defaults[key]
+                new_combined_diff['OLD_OVERRIDES'][key] = overrides[key]
 
-    # Have to recast the values as dictionaries and combine type/value changes
-    new_changed = {}
-    new_changed_types = {}
-    for key in changed:
-        new_changed[key] = dict(changed[key])
-    for key in changed_types:
-        new_changed_types[key] = dict(changed_types[key])
-    new_changed.update(new_changed_types)
-
-    # we make two different yaml dump-able dictionaries, one for new values,
-    # one for old (what is being overridden)
-    new_changed_dict = {}
-    old_changed_dict = {}
-    for key in new_changed:
-        new_changed_dict[key] = new_changed[key]['new_value']
-        old_changed_dict[key] = new_changed[key]['old_value']
-
-    return parsed_diff(added=added, removed=removed,
-                       changed_added=new_changed_dict,
-                       changed_removed=old_changed_dict)
+    return new_combined_diff
 
 
-def main(default_files):
+def main():
     """Manage the work of loading the files and printing the output as needed
 
-    :param default_files: A list of files to load data from
     :return: exits 0
     """
     args = parse_args()
-    combined_defaults = {}
-
-    # load the configs
-    for value in default_files:
-        combined_defaults.update(parsed_yaml_from(value))
-    yaml_overrides = parsed_yaml_from(args.overrides)
 
     # get the actual diffs
-    returned_diff = do_the_diff(combined_defaults, yaml_overrides)
+    returned_combined = do_the_diff(defaults=parsed_yaml_from(args.defaults),
+                                    overrides=parsed_yaml_from(args.overrides))
 
-    # print the diffs
-    if args.danger:
-        print('# Someone used danger-mode, this was not the best idea...')
-        print('---')
-    if returned_diff.added:
-        if args.danger is False:
-            print('==== added the following ====')
-        print(yaml.safe_dump(dict(returned_diff.added),
-                             default_flow_style=False))
-    if args.info and args.danger is False:
-        if returned_diff.removed:
-            print('\n==== removed the following ====')
-            print('==== you shouldn\'t have to care about this... ====')
-            print(yaml.safe_dump(dict(returned_diff.removed),
-                                 default_flow_style=False))
-    if returned_diff.changed_added:
-        if args.danger is False:
-            print('\n==== changed the following (added) ====')
-        print(yaml.safe_dump(returned_diff.changed_added,
-                             default_flow_style=False))
-    if returned_diff.changed_removed and args.danger is False:
-        print('\n==== changed the following (removed) ====')
-        print(yaml.safe_dump(returned_diff.changed_removed,
-                             default_flow_style=False))
+    if os.path.isfile(args.output_file):
+        raise SystemExit('The file you are creating already exists, '
+                         'please move it, we will not overwrite the file.')
+    with open(args.output_file, 'w') as fh:
+        fh.write(yaml.safe_dump(returned_combined,
+                                default_flow_style=False,
+                                explicit_start=True))
+
+    # Add the warrantee sticker
+    print('The output assumes you have been pruning your old overrides '
+          'files as needed and as suggested by release notes.')
+    if 'NEW_DEFAULTS' or 'OLD_OVERRIDES' in returned_combined.keys():
+        print('\nYour output has conflicts, examine it to make sure '
+              'values are as you wish them to be, check "NEW_DEFAULTS" and '
+              '"OLD_OVERRIDES" specifically as you may or may not need '
+              'you\'re overrides as they are.')
 
 
 if __name__ == '__main__':
-    path_prefix = '/opt/rpc-openstack/rpcd/etc/openstack_deploy/'
-    files_to_load = [
-        path_prefix + 'user_osa_variables_defaults.yml',
-        path_prefix + 'user_rpco_variables_defaults.yml'
-    ]
-    main(files_to_load)
+    main()
