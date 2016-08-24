84cc9bd42a7439985c7dc982971943894eeb99f4
Matt Thompson <mattt@defunct.ca>
2016-08-24 14:24:30 -0400

    Remove horizon-extensions cache from repo_all
    This commit does the following:
    
    1. Removes the horizon-extensions clone from the repo servers as the
       repo has changed and cannot be updated using the method implemented
       in repo-install.yml.
    2. Renames upgrade.sh to test-upgrade.sh.  This script will be used for
       testing and gating purposes only, and should not be used for
       production upgrades from Liberty->Mitaka.
    3. Adds a new release note outlining step required to clean up repo
       server.
    
    Connected https://github.com/rcbops/rpc-openstack/issues/1345

--- scripts/test-upgrade.sh
+++ scripts/test-upgrade.sh
@@ -14,5 +14,18 @@
 # limitations under the License.
 #
 # (c) 2015, Nolan Brubaker <nolan.brubaker@rackspace.com>
-echo 'This release does not support upgrading from Liberty.'
-exit 1
+
+set -eux -o pipefail
+
+BASE_DIR=$( cd "$( dirname ${0} )" && cd ../ && pwd )
+OA_DIR="$BASE_DIR/openstack-ansible"
+RPCD_DIR="$BASE_DIR/rpcd"
+
+# TASK #1
+# Bug: https://github.com/rcbops/rpc-openstack/issues/1345
+# Issue: horizon-extensions now gets pulled from a different location; the
+#        git cache on the repo_all containers needs to be deleted so we can
+#        grab the new repo.  Failing to do this will cause repo-install.yml
+#        to fail to run.
+cd ${RPCD_DIR}/playbooks
+ansible -m file -a "path=/var/www/repo/openstackgit/horizon-extensions state=absent" repo_all
