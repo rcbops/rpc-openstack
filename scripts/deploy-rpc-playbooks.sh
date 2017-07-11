59d0623160a7427446cc26702a17dadd9955bc52
Jean-Philippe Evrard <evrardjp@users.noreply.github.com>
2017-07-11 20:19:38 +0100

    Bring the ability to use influxdb (#2388)
    RPC-Maas is able to push metrics to influx, we should expose
    this facility to our users, which includes our gates.
    
    On top of it, we should bring the facility to deploy influx, if
    need be.
    
    RPC-Maas is able to push metrics to influx, we should expose
    this facility to our users, which includes our gates.
    
    Issue: [LA-314](https://rpc-openstack.atlassian.net/browse/LA-314)
    Manual cherry-pick from eae30c4f74ad471cccfd6f17217dc6eeb0aa347b

--- scripts/deploy-rpc-playbooks.sh
+++ scripts/deploy-rpc-playbooks.sh
@@ -9,20 +9,60 @@ source ${BASE_DIR}/scripts/functions.sh
 cd ${RPCD_DIR}/playbooks/
 
 # configure everything for RPC support access
-run_ansible rpc-support.yml
-
-# deploy and configure RAX MaaS
-if [[ "${DEPLOY_MAAS}" == "yes" ]]; then
-  run_ansible setup-maas.yml
+if [[ "${DEPLOY_SUPPORT_ROLE}" == "yes" ]]; then
+    run_ansible rpc-support.yml
 fi
 
 # deploy and configure the ELK stack
 if [[ "${DEPLOY_ELK}" == "yes" ]]; then
-  run_ansible setup-logging.yml
+    run_ansible setup-logging.yml
 fi
 
-# verify RAX MaaS is running after all necessary
-# playbooks have been run
+# Download the latest release of rpc-maas
+# TODO(odyssey4me):
+# Remove this once rpc-gating no longer tries
+# to run rpc-maas as its own thing and instead
+# just uses deploy.sh end-to-end. This line
+# should not be necessary as setup-maas.yml
+# below includes maas-get.yml
+run_ansible maas-get.yml
+
+# Deploy and configure RAX MaaS
 if [[ "${DEPLOY_MAAS}" == "yes" ]]; then
-  run_ansible verify-maas.yml
+    # Run the rpc-maas setup process
+    run_ansible setup-maas.yml
+
+    # verify RAX MaaS is running after all necessary
+    # playbooks have been run
+    run_ansible verify-maas.yml
+fi
+
+# To send data to the influxdb server, we need to deploy and
+# configure telegraf.
+# By default, telegraf will use log_hosts (rsyslog hosts)
+# to define its influxdb servers.
+# These playbooks need maas-get to have run previously.
+if [[ "${DEPLOY_TELEGRAF}" == "yes" ]]; then
+    # BUILD_TAG is used as reference for gating, to allow
+    if [[ -z "${BUILD_TAG}" ]]; then
+        # user_zzz_variables are generated at every build, so
+        # we are fine to just echo it.
+        echo 'maas_job_reference: "${BUILD_TAG}"' >> /etc/openstack_deploy/user_zzz_gating_metrics_variables.yml
+        # Telegraph shipping is done to influx nodes belonging to
+        # influx_telegraf_targets | union(influx_all)
+        if [[ -z "${INFLUX_IP}" ]]; then
+            cat >> /etc/openstack_deploy/user_zzz_gating_metrics_variables.yml << EOF
+influx_telegraf_targets:
+  - 'http://$INFLUX_IP:$INFLUX_PORT'
+EOF
+        fi
+    fi
+    run_ansible /opt/rpc-maas/playbooks/maas-tigkstack-telegraf.yml
+fi
+
+# Deploy Influx
+if [[ "${DEPLOY_INFLUX}" == "yes" ]]; then
+    # We'll assume the deployer has configured his environment
+    # to define the influx_all servers.
+    run_ansible /opt/rpc-maas/playbooks/maas-tigkstack-influxdb.yml
 fi
