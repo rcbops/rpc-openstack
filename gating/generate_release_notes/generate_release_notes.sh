f563eebbc21b708c91cb2c99db3983425966e87f
git-harry <git-harry@live.co.uk>
2017-10-11 15:32:25 +0100

    RE-621 Use repo URL env var with rpc-differ
    This change removes the need to inspect the local repo by using the
    explicitly provided env var `RE_HOOK_REPO_HTTP_URL`.
    
    (cherry picked from commit 9c841fdeeaf40a7ed2370f923ed6ac96e16cc0c5)

--- gating/generate_release_notes/generate_release_notes.sh
+++ gating/generate_release_notes/generate_release_notes.sh
@@ -2,6 +2,5 @@
 
 # This script is run within a docker container to generate release notes.
 
-url=$(git remote -v |awk '/origin.*fetch/{print $2}')
-rpc-differ --debug -r "$url" --update "$PREVIOUS_TAG" "$NEW_TAG" --file notes.rst
+rpc-differ --debug -r "$REPO_URL" --update "$PREVIOUS_TAG" "$NEW_TAG" --file notes.rst
 pandoc --from rst --to markdown_github < notes.rst > notes.md
