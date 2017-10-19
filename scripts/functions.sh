df90d89f282471a4270eb836d6bb6248256b405a
Matthew Thode <mthode@mthode.org>
2017-10-19 13:23:23 -0500

    Make artifacting togglable (#2567)
    Adds a new variable 'DEPLOY_ARTIFACTING', defaulting to 'yes', which
    is used to specify if artifacting is to be used in the deployment.

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -39,6 +39,7 @@ export DEPLOY_SWIFT=${DEPLOY_SWIFT:-"yes"}
 export DEPLOY_HARDENING=${DEPLOY_HARDENING:-"yes"}
 export DEPLOY_RPC=${DEPLOY_RPC:-"yes"}
 export DEPLOY_ARA=${DEPLOY_ARA:-"no"}
+export DEPLOY_ARTIFACTING=${DEPLOY_ARTIFACTING:-"yes"}
 export DEPLOY_SUPPORT_ROLE=${DEPLOY_SUPPORT_ROLE:-"no"}
 export DEPLOY_IRONIC=${DEPLOY_IRONIC:-"no"}
 export BOOTSTRAP_OPTS=${BOOTSTRAP_OPTS:-""}
@@ -117,6 +118,7 @@ function copy_default_user_space_files {
 }
 
 function apt_artifacts_available {
+  [[ "${DEPLOY_ARTIFACTING}" != "yes" ]] && return 1
 
   CHECK_URL="${HOST_RCBOPS_REPO}/apt-mirror/integrated/dists/${RPC_RELEASE}-${DISTRIB_CODENAME}"
 
@@ -129,6 +131,7 @@ function apt_artifacts_available {
 }
 
 function git_artifacts_available {
+  [[ "${DEPLOY_ARTIFACTING}" != "yes" ]] && return 1
 
   CHECK_URL="${HOST_RCBOPS_REPO}/git-archives/${RPC_RELEASE}/requirements.checksum"
 
@@ -141,6 +144,7 @@ function git_artifacts_available {
 }
 
 function python_artifacts_available {
+  [[ "${DEPLOY_ARTIFACTING}" != "yes" ]] && return 1
 
   ARCH=$(uname -p)
   CHECK_URL="${HOST_RCBOPS_REPO}/os-releases/${RPC_RELEASE}/${ID}-${VERSION_ID}-${ARCH}/MANIFEST.in"
@@ -154,6 +158,7 @@ function python_artifacts_available {
 }
 
 function container_artifacts_available {
+  [[ "${DEPLOY_ARTIFACTING}" != "yes" ]] && return 1
 
   CHECK_URL="${HOST_RCBOPS_REPO}/meta/1.0/index-system"
 
