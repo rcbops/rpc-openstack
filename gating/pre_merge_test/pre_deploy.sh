071aa2bb48f36f9da825dd16633f2f5f7b13ddaf
git-harry <git-harry@live.co.uk>
2017-11-20 17:47:50 +0000

    RE-1163 Replace Travis CI with standard jobs
    PR jobs have been created in rpc-gating to run the tests that were the
    responsibility of Travis CI, they are:
    
    - ansible-lint
    - bashate
    - flake8
    - releasenotes
    
    The `pre`, `run` and `post` scripts now check the standard environment
    variable `RE_JOB_ACTION`, to determine the type of build that is
    running. If the variable is set to `"tox-test"` a tox environment is
    requested and the specific one is defined in `RE_JOB_SCENARIO` otherwise
    the scripts default to their original behaviour of deploying RPCO.
    
    (cherry picked from commit 0f903764bac890b507de29824a81ee47c6fbacb9)

---
+++ gating/pre_merge_test/pre_deploy.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+## Shell Opts ----------------------------------------------------------------
+
+set -e -u -x
+set -o pipefail
+
+## Vars ----------------------------------------------------------------------
+export KIBANA_SELENIUM_REPO="https://github.com/rcbops-qe/kibana-selenium"
+export KIBANA_SELENIUM_BRANCH="mitaka"
+
+## Main ----------------------------------------------------------------------
+
+# If the current folder's basename is rpc-openstack then we assume
+# that it is the root of the git clone. If the git clone is not in
+# /opt then we symlink the current folder there so that all the
+# rpc-openstack scripts work as expected.
+if [[ "$(basename ${PWD})" == "rpc-openstack" ]]; then
+  if [[ "${PWD}" != "/opt/rpc-openstack" ]]; then
+    ln -sfn ${PWD} /opt/rpc-openstack
+  fi
+fi
+
+# Clone the kibana-selenium git repository
+git clone -b ${KIBANA_SELENIUM_BRANCH} ${KIBANA_SELENIUM_REPO} /opt/kibana-selenium
+
+# Prepare for Kibana Selenium Tests
+cd /opt/kibana-selenium
+
+# The phantomjs package on 16.04 is buggy, see:
+# https://github.com/ariya/phantomjs/issues/14900
+# https://bugs.launchpad.net/ubuntu/+source/phantomjs/+bug/1578444
+#apt-get install -y phantomjs
+apt-get install -y fontconfig
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+tar -xjf phantomjs-2.1.1-linux-x86_64.tar.bz2
+if [[ ! -d ".venv" ]]; then
+  pip install virtualenv
+  virtualenv .venv
+fi
+
+# Work around https://github.com/pypa/virtualenv/issues/1029
+export VIRTUAL_ENV_DISABLE_PROMPT=true
+
+set +x; source .venv/bin/activate; set -x
+
+if [ -f ~/.pip/pip.conf ]; then
+  mv ~/.pip/pip.conf ~/.pip/pip.conf.bak
+  pip install -r requirements.txt
+  mv ~/.pip/pip.conf.bak ~/.pip/pip.conf
+else
+  pip install -r requirements.txt
+fi
