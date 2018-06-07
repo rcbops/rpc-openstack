0fe0f25aedcc3e646ddcd090e8115995d53f3136
Jesse Pretorius <jesse.pretorius@rackspace.co.uk>
2018-06-07 22:35:14 +0100

    RE-1466 Ensure the apt cache is updated
    Before we can install the fontconfig package, we need to
    update the apt cache so that apt is aware of where it
    can be installed from.
    
    This is required in order to make use of nodepool nodes
    which do not come with an updated cache when handed over
    to tests.

--- gating/pre_merge_test/pre_deploy.sh
+++ gating/pre_merge_test/pre_deploy.sh
@@ -31,6 +31,7 @@ cd /opt/kibana-selenium
 # https://github.com/ariya/phantomjs/issues/14900
 # https://bugs.launchpad.net/ubuntu/+source/phantomjs/+bug/1578444
 #apt-get install -y phantomjs
+apt-get update
 apt-get install -y fontconfig
 wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
 tar -xjf phantomjs-2.1.1-linux-x86_64.tar.bz2
