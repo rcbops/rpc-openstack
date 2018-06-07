9353eae4ea34ff1a1bd956213d26a6871bedc1fe
Jesse Pretorius <jesse.pretorius@rackspace.co.uk>
2018-06-07 19:03:52 +0100

    RO-4265 Update reno hook to support OSA fork
    With newton upstream gone EOL, we need to use a fork
    of the openstack/openstack-ansible repo. Until now
    the rpc-differ/osa-differ repositories have not
    supported doing this. This patch updates the versions
    of the tooling used to support the option and changes
    the hook to use the new CLI option.

--- gating/generate_release_notes/generate_commit_diff_notes.sh
+++ gating/generate_release_notes/generate_commit_diff_notes.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xe
 
-rpc-differ --debug -r "$REPO_URL" --update "$PREVIOUS_TAG" "$NEW_TAG" --file diff_notes.rst
+rpc-differ --debug -r "$REPO_URL" --update --osa-repo-url https://github.com/rcbops/openstack-ansible "$PREVIOUS_TAG" "$NEW_TAG" --file diff_notes.rst
 pandoc --from rst --to markdown_github < diff_notes.rst > diff_notes.md
