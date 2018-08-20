b8209e934c8a13aea62a4c712b5b99ed5c1fa02f
Jesse Pretorius <jesse.pretorius@rackspace.co.uk>
2018-08-20 15:51:27 +0100

    Use a common gating script for gating host prep
    Rather than repeat the preparation of apt sources
    and the installation of python/python-yaml in
    several locations, we consolidate the necessary
    preparation into a common script.
    
    Part of this includes ensuring that we use the
    Rackspace apt mirrors. This is a necessary change
    specifically for MNAIO tests where they are setup
    to use archive.ubuntu.com which is slow and fails
    often due to there being more hops to it.
    
    JIRA: RE-1730
    (cherry picked from commit c3674b33ec550fa21dc5d248a2bc4f15a70898ab)

--- gating/check/run_deploy.sh
+++ gating/check/run_deploy.sh
@@ -9,15 +9,6 @@ set -o pipefail
 
 export DEPLOY_AIO="true"
 
-# In order to get the value for rpc_release, we need to use
-# a python script, so we need to make sure that python and
-# the python yaml library are installed. If not, the value
-# for rpc_release cannot be read and the check for the value
-# will result in a null return, causing all artifacts to be
-# disabled.
-apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y python-minimal python-yaml
-
 # When a PR contains a change to rpc_release, there may not
 # be any artifacts for the new release version. If this is
 # the case then we need to disable the artifact usage. In
