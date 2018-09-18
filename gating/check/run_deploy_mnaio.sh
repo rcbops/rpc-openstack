52bb07e209c3a2ca8bff9ae6e49214bdb8cf11a1
Jesse Pretorius <jesse.pretorius@rackspace.co.uk>
2018-09-18 17:50:54 +0100

    De-duplicate gating var exposure
    The env vars setting is now done in the common script,
    and the environment exposure is already done in run,
    so we remove it from the run_deploy_mnaio script.

--- gating/check/run_deploy_mnaio.sh
+++ gating/check/run_deploy_mnaio.sh
@@ -19,24 +19,10 @@
 set -exu
 
 echo "Building a Multi Node AIO (MNAIO)"
-echo "+-------------------- MNAIO ENV VARS --------------------+"
-env
-echo "+-------------------- MNAIO ENV VARS --------------------+"
 
 ## Vars ----------------------------------------------------------------------
 
-# These vars are set by the CI environment, but are given defaults
-# here to cater for situations where someone is executing the test
-# outside of the CI environment.
-export RE_JOB_NAME="${RE_JOB_NAME:-}"
-export RE_JOB_IMAGE="${RE_JOB_IMAGE:-}"
-export RE_JOB_BRANCH="${RE_JOB_BRANCH:-master}"
-export RE_JOB_SCENARIO="${RE_JOB_SCENARIO:-swift}"
-export RE_JOB_ACTION="${RE_JOB_ACTION:-deploy}"
-export RE_JOB_FLAVOR="${RE_JOB_FLAVOR:-}"
-export RE_JOB_TRIGGER="${RE_JOB_TRIGGER:-PR}"
-export RE_HOOK_ARTIFACT_DIR="${RE_HOOK_ARTIFACT_DIR:-/tmp/artifacts}"
-export RE_HOOK_RESULT_DIR="${RE_HOOK_RESULT_DIR:-/tmp/results}"
+source "$(readlink -f $(dirname ${0}))/../gating_vars.sh"
 
 ## Functions -----------------------------------------------------------------
 source /opt/rpc-openstack/scripts/functions.sh
