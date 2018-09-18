e74cc2c53269b65a250cbb8d773973f894b9ff1c
Jesse Pretorius <jesse.pretorius@rackspace.co.uk>
2018-09-18 12:42:41 +0100

    Use common vars for gating scripts
    To make testing the same way as the gate simpler outside
    of the gate, we implement a common vars script which is
    sourced by the pre/run/post stages, ensuring that all vars
    required for each stage are available.
    
    (cherry picked from commit ee0b7798d04866294113461146cb33e959e1b126)

---
+++ gating/gating_vars.sh
@@ -0,0 +1,11 @@
+# These vars are set by the CI environment, but are given defaults
+# here to cater for situations where someone is executing the test
+# outside of the CI environment.
+export RE_JOB_IMAGE="${RE_JOB_IMAGE:-xenial_no_artifacts}"
+export RE_JOB_SCENARIO="${RE_JOB_SCENARIO:-swift}"
+export RE_JOB_ACTION="${RE_JOB_ACTION:-deploy}"
+export RE_JOB_FLAVOR="${RE_JOB_FLAVOR:-}"
+export RE_JOB_TRIGGER="${RE_JOB_TRIGGER:-USER}"
+export RE_HOOK_ARTIFACT_DIR="${RE_HOOK_ARTIFACT_DIR:-/tmp/artifacts}"
+export RE_HOOK_RESULT_DIR="${RE_HOOK_RESULT_DIR:-/tmp/results}"
+export RE_JOB_NAME="${RE_JOB_NAME:-${RE_JOB_TRIGGER}_rpc-openstack-master-${RE_JOB_IMAGE}_no_artifacts-${RE_JOB_SCENARIO}-${RE_JOB_ACTION}}"
