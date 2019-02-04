97e5b1c073ca8b0bba39066022d3d59cc2ef2d12
git-harry <git-harry@live.co.uk>
2019-02-04 15:44:05 +0000

    Fix releases notes generation for major releases
    This change ensure that the old commit used by rpc-differ is paired with
    the correct product release and Ansible role requirements file.
    
    JIRA: RE-2353

--- gating/generate_release_notes/generate_commit_diff_notes.sh
+++ gating/generate_release_notes/generate_commit_diff_notes.sh
@@ -6,8 +6,18 @@ export BASE_DIR="${PWD}"
 # Source the functions
 source "${BASE_DIR}/scripts/functions.sh"
 
+
+if echo ${PREVIOUS_TAG} | egrep '^r[0-9]+\.'; then
+  PREVIOUS_RPC_PRODUCT_RELEASE="$(component release --component-name rpc-openstack get --version ${PREVIOUS_TAG} | grep 'series:' | awk '{print $2}')"
+else
+  # Assume PREVIOUS_TAG is one of the release branches
+  PREVIOUS_RPC_PRODUCT_RELEASE="${PREVIOUS_TAG}"
+fi
+
 rpc-differ \
   --debug \
+  -rpoc ${PREVIOUS_RPC_PRODUCT_RELEASE} \
+  -rroc "ansible-role-${PREVIOUS_RPC_PRODUCT_RELEASE}-requirements.yml" \
   -rp ${RPC_PRODUCT_RELEASE} \
   -rr "ansible-role-${RPC_PRODUCT_RELEASE}-requirements.yml" \
   -r "$REPO_URL" \
