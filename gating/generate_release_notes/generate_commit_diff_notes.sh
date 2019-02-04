72eeae0a9fb3cff3b0ad6ca62a669cbbfd62247a
git-harry <git-harry@live.co.uk>
2019-02-04 14:26:47 +0000

    Improve readability of rpc-differ arguments

--- gating/generate_release_notes/generate_commit_diff_notes.sh
+++ gating/generate_release_notes/generate_commit_diff_notes.sh
@@ -6,5 +6,13 @@ export BASE_DIR="${PWD}"
 # Source the functions
 source "${BASE_DIR}/scripts/functions.sh"
 
-rpc-differ --debug -rp ${RPC_PRODUCT_RELEASE} -rr "ansible-role-${RPC_PRODUCT_RELEASE}-requirements.yml" -r "$REPO_URL" --update "$PREVIOUS_TAG" "$NEW_TAG" --file diff_notes.rst
+rpc-differ \
+  --debug \
+  -rp ${RPC_PRODUCT_RELEASE} \
+  -rr "ansible-role-${RPC_PRODUCT_RELEASE}-requirements.yml" \
+  -r "$REPO_URL" \
+  --update \
+  "$PREVIOUS_TAG" \
+  "$NEW_TAG" \
+  --file diff_notes.rst
 pandoc --from rst --to markdown_github < diff_notes.rst > diff_notes.md
